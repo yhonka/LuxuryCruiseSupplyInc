9ea9f1e8965cfada405169f3a4a1b05301be1af2
yhonka <82457246+yhonka@users.noreply.github.com>
2024-05-22 12:22:27 -0400

    se termino de completar los servicios

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -20,5 +20,24 @@ $email_body = "You have received a new message from your website contact form.\n
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
-return true;			
-?>
+return true;	
+
+
+// <?php
+
+//     $destinatario = 'yhonka.yjml@gmail.com';
+    
+//     $nombre = $_POST['name'];
+//     $mensaje = $_POST['message'];
+//     $email = $_POST['email'];
+//     $email_subject = "PRUEBA WEB:  $name";
+//     $header = "Enviado desde la pagina test.com";
+//     $mensajeCompleto = "Correo: " . $email  . "\nAtentamente: " . $nombre . " " . "\nMensaje: " . $mensaje ;
+    
+//     mail($destinatario, $email_subject  utf8_decode($mensajeCompleto), $header );
+    
+//     echo" <script>alert('Email send')</script>";
+//     echo" <script>setTimeout(\"location.href='index.html'\",100)</script>";
+
+?> 
+
